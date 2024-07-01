2b241184689cc5aaf420696bbc979ef5b353f612
bengold1999 <bengold1999@gmail.com>
2024-07-01 08:16:05 +0300

    Add base home page

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 
 const Home = () => {
-  return (
-    <div>
-      Home
-    </div>
-  )
+    return (
+        <section className='w-full flex-center flex-col'>
+            <h1 className='head_text'>Discover and share
+                <br className='max-md:hidden' />
+                <span className='green_gradient text-center'>AI prompts</span>
+
+            </h1>
+            <p className='desc text-center'>Prompts Web is an open-source AI prompting tool for modern world to discover, create and share creative prompts</p>
+
+
+        </section>
+    )
 }
 
 export default Home
