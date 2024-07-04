02e273e9743db535e4eeb14ffa2c80bc033c8b9d
bengold1999 <bengold1999@gmail.com>
2024-07-04 15:21:24 +0300

    Add input to Feed component

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,10 +1,49 @@
-import React from 'react'
+'use client'
+
+import { useState, useEffect } from 'react'
+
+import PromotCard from './PromotCard'
+
+const PromotCardList =({data , handleTagClick}) => {
+
+  return (  
+    <div className='mt-16'>
+      {data?.map((post) => (
+        <PromotCard
+          key={post._id}
+          post={post}
+        />
+      ))}
+    </div>
+  )
+}
+
 
 const Feed = () => {
+
+  const [search, setSearch] = useState('')
+  const handleSearchChange = (e) => {
+    // setSearch(e.target.value)
+  }
+
+  useEffect(() => {
+    
+  }, [])
+
   return (
-    <div>
-      
-    </div>
+    <section className='feed'>
+      <form className='relative w-full flex-center'>
+        <input type="text"
+          placeholder="Search for a tag or a username"
+          className='search_input'
+          value={search}
+          onChange={handleSearchChange}
+          required
+        />
+      </form>
+
+      <PromotCardList />
+    </section >
   )
 }
 
