3420ef40922fcbd1ecfd8102a029400aa02ae063
bengold1999 <bengold1999@gmail.com>
2024-07-06 15:55:51 +0300

    Add style to web

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -39,7 +39,7 @@ const Nav = () => {
             </Link>
             <button type='button' className='outline_btn' >{signOut}Sign Out</button>
             <Link href='/profile'>
-              <Image src='/assets/images/logo.svg' width={37} height={37} className='rounded-full' alt='profile' />
+              <Image src={session?.user.image} width={37} height={37} className='rounded-full' alt='profile' />
             </Link>
           </div>
         ) : (
