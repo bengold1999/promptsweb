c14f7fdbfae78dd62cac09e8c28967aa78f2b853
bengold1999 <bengold1999@gmail.com>
2024-07-08 10:22:15 +0300

    better connect tp db

--- utils/database.js
+++ utils/database.js
@@ -1,30 +1,27 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
-let isConnected = false;
+let isConnected = false; 
 
 export const connectToDB = async () => {
-    mongoose.set("strictQuery", true);
+  mongoose.set('strictQuery', true);
 
-    if (isConnected) {
-        console.log("MongoDB is already connected");
-        return;
-    }
+  if(isConnected) {
+    console.log('MongoDB is already connected');
+    return;
+  }
 
-    const mongoUri = process.env.MONGODB_URI;
-    if (!mongoUri) {
-        throw new Error('MONGODB_URI environment variable is not defined');
-    }
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      dbName: "promts-web",
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
 
-    try {
-        await mongoose.connect(mongoUri, {
-            dbName: "promts-web",
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
+    isConnected = true;
 
-        isConnected = true;
-        console.log("MongoDB connected");
-    } catch (error) {
-        console.log("Failed to connect to MongoDB", error);
-    }
+    console.log('MongoDB connected')
+  } catch (error) {
+    console.log(error);
+  }
 }
+
